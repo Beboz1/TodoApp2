b7d5aa7a5981fbbd158925f3fe06ade26233ecc1
Beboz1 <dokabanoub@gmail.com>
2024-09-18 16:11:51 -0900

    setup
    this is just the starting files

---
+++ app/src/components/Todo.jsx
@@ -0,0 +1,20 @@
+import "./todo.css"
+function Todo(){
+    return(
+        <div className="container">
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+            <div><input className="title" type="text" /><input className="description" type="text" /></div>
+        </div>
+    )
+}
+export default Todo
